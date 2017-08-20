0db1b05ddd933fda89fadfb1d0362ceb75d0155e
Walid Badawi <walidkhb@gmail.com>
2017-08-20 22:19:18 +0300

    update securit

--- src/main/java/com/stm/config/SpringSecurityConfig.java
+++ src/main/java/com/stm/config/SpringSecurityConfig.java
@@ -1,18 +1,57 @@
 package com.stm.config;
 
+import javax.annotation.Resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 
 @Configuration
 // http://docs.spring.io/spring-boot/docs/current/reference/html/howto-security.html
 // Switch off the Spring Boot security configuration
 //@EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	private final Log log = LogFactory.getLog(SpringSecurityConfig.class);
+
+
+
+	@Value("${rememberMe.privateKey}")
+	private String rememberMeKey;
+    
+	@Resource
+	private UserDetailsService userService;
+    
+    @Bean
+    public RememberMeServices rememberMeServices() {
+    	
+        TokenBasedRememberMeServices rememberMeServices = new TokenBasedRememberMeServices(rememberMeKey, userService);
+        
+        // See http://stackoverflow.com/questions/25565809/implementing-a-remember-me-for-spring-social
+        rememberMeServices.setAlwaysRemember(true);
+        
+        return rememberMeServices;
+        
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+      log.info("Creating password encoder bean");
+      return new BCryptPasswordEncoder();
+    }
+
 
     @Autowired
     private AccessDeniedHandler accessDeniedHandler;
@@ -66,4 +105,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
       web.ignoring().antMatchers("/public/**/**/**","/fonts/**");
     }
+    @Autowired
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
+        authManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
 }
