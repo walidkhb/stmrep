fe67a0ae1b845ded8e19c51ae40d175eed9d4888
Walid Badawi <walidkhb@gmail.com>
2017-08-21 13:18:17 +0300

    shipment

--- src/main/java/com/stm/service/ShipmentService.java
+++ src/main/java/com/stm/service/ShipmentService.java
@@ -1,5 +1,30 @@
 package com.stm.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.stm.data.entity.Shipment;
+
 public interface ShipmentService {
 
+	void saveShipment(Shipment shipment);
+
+	void updateShipment(Shipment shipment);
+
+	void deleteShipmentById(Long id);
+
+	void deleteAllShipments();
+
+	List<Shipment> findAllShipments();
+	 Iterable<Shipment> listAllShipments();
+
+	    Shipment getShipmentById(Long id);
+
+	   
+
+	    void deleteShipment(Long id);
+
+	    Page<Shipment> findAll(Pageable pageable);
 }
