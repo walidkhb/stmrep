f1f1a369fcc6a82242b9c7759b60ca7d3185f6f3
aseel <aseel.l.waleed@gmail.com>
2017-08-21 13:28:34 +0300

    cont

--- src/main/java/com/stm/service/ContractService.java
+++ src/main/java/com/stm/service/ContractService.java
@@ -1,5 +1,24 @@
 package com.stm.service;
 
-public interface ContractService {
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.stm.data.entity.Contract;
+
+public interface ContractService {
+	public abstract List<Contract> getAll();
+	public abstract Contract addContract(Contract contract);
+	public abstract Contract findOne(Long contractId);
+	Iterable<Contract> listAllContracts();
+
+	   Contract getContractById(Long id);
+
+	  
+
+	   void deleteContract(Long id);
+
+	   Page<Contract> findAll(Pageable pageable);
+	   
 }
