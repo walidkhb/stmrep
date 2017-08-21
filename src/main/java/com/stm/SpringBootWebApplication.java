79036e6024449a6b849837d29c94873037f69599
Walid Badawi <walidkhb@gmail.com>
2017-08-21 12:34:59 +0300

    test 3

--- src/main/java/com/stm/SpringBootWebApplication.java
+++ src/main/java/com/stm/SpringBootWebApplication.java
@@ -10,7 +10,7 @@ import com.stm.data.entity.User;
 import com.stm.service.GroupServiceImpl;
 import com.stm.service.UserServiceImpl;
 /*
- * test 1 2
+ * test 1 2 3
  */
 @SpringBootApplication
 public class SpringBootWebApplication {
